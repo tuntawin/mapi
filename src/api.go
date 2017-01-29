7e49fe3b4cf918a47619338211eb9cc35c484525
tuntawin <teerapong.cs@gmail.com>
2017-01-29 16:24:44 +0700

    test

--- src/api.go
+++ src/api.go
@@ -4,20 +4,32 @@ import "net/http"
 import "github.com/labstack/echo"
 import "github.com/labstack/echo/middleware"
 
+//User lkjfhlg
+type User struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	UserName  string `json:"username"`
+	Password  string `json:"password"`
+}
 
 func index(c echo.Context) error {
-  return c.JSON(http.StatusOK, "Hello World")
+	return c.JSON(http.StatusOK, "Hello World")
 }
 
 func grtUsers(c echo.Context) error {
-  firstname := "Teerapong"
-  return c.JSON(http.StatusOK, firstname)
+	guy := User{
+		"teerapong",
+		"tuntawin",
+		"Guy",
+		"123",
+	}
+	return c.JSON(http.StatusOK, guy)
 }
 
 func main() {
-  e := echo.New()
-  e.Use(middleware.Logger())
-  e.GET("/", index)
-  e.GET("/users", grtUsers)
-  e.Logger.Fatal(e.Start(":1323"))
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.GET("/", index)
+	e.GET("/users", grtUsers)
+	e.Logger.Fatal(e.Start(":1323"))
 }
