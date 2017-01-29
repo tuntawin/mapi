a8a1183bcd36402335ea6ddef34806b8a3d54fc9
tuntawin <teerapong.cs@gmail.com>
2017-01-29 16:24:54 +0700

    s

--- src/api.go
+++ src/api.go
@@ -6,30 +6,45 @@ import "github.com/labstack/echo/middleware"
 
 //User lkjfhlg
 type User struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-	UserName  string `json:"username"`
-	Password  string `json:"password"`
+	Firstname string `json:"firstname,omitempty"`
+	Lastname  string `json:"lastname,omitempty"`
+	UserName  string `json:"username,omitempty"`
+	Password  string `json:"password,omitempty"`
 }
 
 func index(c echo.Context) error {
 	return c.JSON(http.StatusOK, "Hello World")
 }
 
-func grtUsers(c echo.Context) error {
+func getUsers(c echo.Context) error {
 	guy := User{
 		"teerapong",
 		"tuntawin",
 		"Guy",
-		"123",
+		"",
 	}
 	return c.JSON(http.StatusOK, guy)
+}
+
+func getUserByID(c echo.Context) error {
+	id := c.Param("id")
+	return c.JSON(http.StatusOK, id)
+}
+
+func saveUser(c echo.Context) error {
+	user := User{}
+	if err := c.Bind(user); err != nil {
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+	return c.JSON(http.StatusOK, "post")
 }
 
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.GET("/", index)
-	e.GET("/users", grtUsers)
+	e.GET("/users", getUsers)
+	e.GET("/users/:id", getUserByID)
+	e.POST("users", saveUser)
 	e.Logger.Fatal(e.Start(":1323"))
 }
